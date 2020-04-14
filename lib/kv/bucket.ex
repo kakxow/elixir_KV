6833f96dc7c5f82cfb652939b498ca16a1e299f0
kakxow <kakxow@gmail.com>
2020-04-14 21:51:46 +0300

    registry, supervisors and ETS

--- lib/kv/bucket.ex
+++ lib/kv/bucket.ex
@@ -1,5 +1,5 @@
 defmodule KV.Bucket do
-  use Agent
+  use Agent, restart: :temporary
 
   @doc """
   Starts a new bucket.
@@ -11,7 +11,22 @@ defmodule KV.Bucket do
   @doc """
   Gets a value from the `bucket` by `key`.
   """
+  def get(bucket, key) do
+    Agent.get(bucket, &Map.get(&1, key))
+  end
+
+  @doc """
+  Puts a `value` for the given `key` in the `bucket`.
+  """
   def put(bucket, key, value) do
-    Agent.update(bucket, &Map.__info__put(&1, key, value))
+    Agent.update(bucket, &Map.put(&1, key, value))
+  end
+
+  @doc """
+  Deletes `key` from `bucket`.
+  Returns the current value of `key`, if `key` exists.
+  """
+  def delete(bucket, key) do
+    Agent.get_and_update(bucket, &Map.pop(&1, key))
   end
 end
--- test/kv/bucket_test.exs
+++ test/kv/bucket_test.exs
@@ -1,11 +1,32 @@
 defmodule KV.BucketTest do
   use ExUnit.Case, async: true
 
-  test "stores values by key" do
-    {:ok, bucket} = KV.Bucket.start_link([])
-    assert KV.Bucket.get(bucket, "milk") == nil
+  setup do
+    bucket = start_supervised!(KV.Bucket)
+    %{bucket: bucket}
+  end
 
+  test "test bucket is empty", %{bucket: bucket} do
+    assert KV.Bucket.get(bucket, "milk") == nil
+  end
+
+  test "test bucket get", %{bucket: bucket} do
     KV.Bucket.put(bucket, "milk", 3)
     assert KV.Bucket.get(bucket, "milk") == 3
+  end
+
+  test "test bucket.delete returns", %{bucket: bucket} do
+    KV.Bucket.put(bucket, "vodka", 42)
+    assert KV.Bucket.delete(bucket, "vodka") == 42
+  end
+
+  test "test bucket.delete deletes", %{bucket: bucket} do
+    KV.Bucket.put(bucket, "vodka", 42)
+    KV.Bucket.delete(bucket, "vodka")
+    assert KV.Bucket.get(bucket, "vodka") == nil
+  end
+
+  test "are temporary workerks" do
+    assert Supervisor.child_spec(KV.Bucket, []).restart == :temporary
   end
 end
